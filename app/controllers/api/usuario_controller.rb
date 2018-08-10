830c46d5331cf3852e3a4d859f1cc0c5142c9f83
rmartinezalf <rmartinezalf@gamil.com>
2018-08-09 20:37:04 -0500

    primera api

--- app/controllers/api/usuario_controller.rb
+++ app/controllers/api/usuario_controller.rb
@@ -1,2 +1,29 @@
 class Api::UsuarioController < ApplicationController
+    before_action :set_usuario, only: [:show, :update, :destroy]
+    def index
+      @usuario = Usuario.all
+      render json: @usuario
+    end 
+    def create
+      @usuario = Usuario.new(usuario_params)
+      if @usuario.save
+        render json: @usuario, status: :created
+      else
+        render json: @usuario.errors, status: :unprocessable_entity
+      end
+    end 
+    def update
+      if @usuario.update(usuario_params)
+        render json: @usuario, status: :ok
+      else
+        render json: @usuario.errors, status: :unprocessable_entity
+      end
+    end
+    private
+      def set_usuario
+        @usuario = Usuario.find(params[:id])
+      end
+      def usuario_params    
+         params.permit(:telefono, :avatar, :localidad, :estado, :alias)      
+      end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  namespace :api do
-    resources :usuarios, only: [:index, :create, :destroy, :update, :show]  
+  namespace :api, defaults: {format: 'json'} do
+    resources :usuario, only: [:index, :create, :destroy, :update, :show]  
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
